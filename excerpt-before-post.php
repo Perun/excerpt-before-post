5000022b7f1ad04b4943918a631e2dee0b0df072
Vladimir Simović <info@perun.net>
2017-12-06 08:33:12 +0100

    Create excerpt-before-post.php

---
+++ excerpt-before-post.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Excerpt ausgeben
+Plugin URI: http://www.perun.net
+Description: If there is a manual excerpt of the article, it should be output before the actual content..
+Version: 0.1
+Author: Vladimir Simović
+Author URI: http://www.perun.net
+*/
+
+function excerpt_before_content($content) {
+    if (is_single() && has_excerpt()) {
+        $subtitle = get_the_excerpt();
+        $subtitle_first .= "<p class=\"excerpt\">" . $subtitle . "</p>\n";
+        $subtitle_first .= $content;
+        return $auszug_zuerst;
+        } else {
+            return $content;
+        }
+}
+add_filter( 'the_content', 'excerpt_before_content' );
+?>
